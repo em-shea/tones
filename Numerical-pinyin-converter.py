fddc6761b871aee54e690e0110baac8a6b3b2b0b
em-shea <c.emilyshea@gmail.com>
2019-02-10 11:46:43 -0800

    Tone placement logic completed

--- Numerical-pinyin-converter.py
+++ Numerical-pinyin-converter.py
@@ -1,6 +1,6 @@
-DEBUG_ENABLED = False
+DEBUG_ENABLED = True
 
-test_word = 'hao4 fei4' #'bang3 jia4' #'nv3 er2'
+test_word = 'lve4 duo2' #'hu2 shuo1' # 'hao4 fei4' #'bang3 jia4' #'nv3 er2' #jin3 kuai4 jian3 shao3 huang1 miu4 
 
 pinyin = {
     'a': ['ā', 'á', 'ǎ', 'à', 'a'], 
@@ -8,7 +8,7 @@ pinyin = {
     'i': ['ī', 'í', 'ǐ', 'ì', 'i'], 
     'o': ['ō', 'ó', 'ǒ', 'ò', 'o'], 
     'u': ['ū', 'ú', 'ǔ', 'ù', 'u'], 
-    'v': ['ǖ', 'ǘ', 'ǚ', 'ǜ', 'ü']
+    'ü': ['ǖ', 'ǘ', 'ǚ', 'ǜ', 'ü']
 }
 
 def debug(*args, **kwargs):
@@ -27,11 +27,12 @@ def convert_to_numerical_pinyin(word):
 
 def convert_indiv_character(indiv_character):
 
-    letter_list = list(indiv_character)
-
     debug("")
     debug("------")
-    debug("Starting loop for word", letter_list)
+    debug("Starting loop for word:", indiv_character)
+
+    # Convert indiv char string into list of letters
+    letter_list = list(indiv_character)
 
     # Select tone as last item in letter_list
     # Set integer to look up tone in list by  
@@ -40,25 +41,37 @@ def convert_indiv_character(indiv_character):
 
     debug("Found tone", tone)
 
+    for index, char in enumerate(letter_list):
+        if char in 'v':
+            letter_list[index] = 'ü'
+    debug(letter_list)
+
     counter = 0
     vowels = []
-
+    
     for index, char in enumerate(letter_list):
-        if char in 'aeiouv':
+        if char in 'aeiouü':
             counter = counter + 1
             vowels.append(char)
     debug("Found vowels:", vowels)
 
     debug("Checking for multiple vowels")
+    
     if counter > 1:
         debug("Found multiple vowels, count:", counter)
+
         if 'a' in vowels:
             tone_vowel = 'a'
-            debug("Found multiple vowels", vowels, "Selected", tone_vowel)
-        if 'e' in vowels:
+        elif 'o' in vowels:
+            tone_vowel = 'o'
+        elif 'e' in vowels:
             tone_vowel = 'e'
+        else: 
+            tone_vowel = vowels[1]
+
+        debug("Selected vowel:", tone_vowel)    
     else:
-        tone_vowel = ''.join(vowels)
+        tone_vowel = vowels[0]
         debug("Only one vowel found:", tone_vowel)
 
     tonal_pinyin = pinyin[tone_vowel][tone_int]
