a1762f78a9de18c2a531546366db7f6ccd46c2ce
em-shea <c.emilyshea@gmail.com>
2019-02-10 18:51:35 -0800

    add dictwriter to csv

--- csv_wrapper.py
+++ csv_wrapper.py
@@ -1,9 +1,17 @@
 from numerical_pinyin_converter import convert_from_numerical_pinyin
 import csv
 
-with open('hsk_level_6.csv') as fh:
+with open('hsk_level_6.csv', encoding ="utf8") as fh:
   reader = csv.DictReader(fh)
+  new_word_list = []
   for row in reader:
-    numerical_pinyin_word = row['Pronunciation']
-    new_word = convert_from_numerical_pinyin(numerical_pinyin_word)
-    print(new_word)
+    row['Pronunciation'] = convert_from_numerical_pinyin(row['Pronunciation'])
+    new_word_list.append(row)
+
+with open('hsk_level_6_modified.csv', 'w', newline='', encoding ="utf8") as fh:
+  fieldnames = list(new_word_list[0].keys())
+  writer = csv.DictWriter(fh, fieldnames=fieldnames)
+  writer.writeheader()
+  
+  for row in new_word_list:
+    writer.writerow(row)
