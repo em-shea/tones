5f9d0a4ea4209191c04a2bdebce88133e71dae05
Shea <emshea@amazon.com>
2019-02-11 12:39:03 -0800

    add cli tool

---
+++ cli.py
@@ -0,0 +1,19 @@
+from numerical_pinyin_converter import convert_from_numerical_pinyin
+import time
+
+print("Welcome to the numerical pinyin converter.")
+
+print("")
+
+print("Input numerical pinyin (tai4 kong1) and get back pinyin with tone marks (tài kōng).")
+print("Examples of valid numerical pinyin: huang1 miu4, lve4 duo2, tai4 tai5, xian4 r5")
+
+print("")
+
+while True:
+    pinyin = input("Enter numerical pinyin: ")
+    try:
+        print(convert_from_numerical_pinyin(pinyin))
+    except Exception as e:
+        print("Sorry, please enter valid pinyin (ex: huang1 miu4, lve4 duo2, tai4 tai5, xian4 r5)")
+       # print(e)
--- csv_example/csv_wrapper.py
+++ csv_example/csv_wrapper.py
@@ -1,13 +1,21 @@
+# Add parent directory with numerical_pinyin_converter.py to path
+import os.path, sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
+
 from numerical_pinyin_converter import convert_from_numerical_pinyin
 import csv
 
+# Open desired file, in this case hsk_level_6.csv
 with open('hsk_level_6.csv', encoding ="utf8") as fh:
   reader = csv.DictReader(fh)
   new_word_list = []
   for row in reader:
+    
+    # Run pinyin converter function on each value for the Pronunciation key
     row['Pronunciation'] = convert_from_numerical_pinyin(row['Pronunciation'])
     new_word_list.append(row)
 
+# Create and write new file, in this case called hsk_level_6_modified.csv
 with open('hsk_level_6_modified.csv', 'w', newline='', encoding ="utf8") as fh:
   fieldnames = list(new_word_list[0].keys())
   writer = csv.DictWriter(fh, fieldnames=fieldnames)
